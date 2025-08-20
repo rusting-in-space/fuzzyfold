f9f0c2c21e5c36ed2e0e03127a1760c3cbad4b86
Stefan Badelt <bad-ants-fleet@posteo.eu>
2025-08-20 19:06:37 +0200

    first code of loop structure modifications. needs testing!

--- crates/kinetics/src/loop_decomposition.rs
+++ crates/kinetics/src/loop_decomposition.rs
@@ -1,5 +1,6 @@
 
 use structure::PairTable;
+use std::ops::Range;
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub enum NearestNeighborLoop {
@@ -32,6 +33,222 @@ impl NearestNeighborLoop {
                 0 => Self::Hairpin { closing: (i, j) },
                 1 => Self::Interior { closing: (i, j), inner: branches[0] },
                 _ => Self::Multibranch { closing: (i, j), branches },
+            },
+        }
+    }
+
+    pub fn closing(&self) -> Option<(usize, usize)> {
+        match self { Self::Hairpin { closing }
+            | Self::Interior { closing, .. }
+            | Self::Multibranch { closing, .. } => Some(*closing),
+            Self::Exterior { .. } => None,
+        }
+    }
+
+    fn unpaired_ranges(&self, len: usize) -> Vec<Range<usize>> {
+        match self {
+            Self::Hairpin { closing: (i, j) } => { 
+                vec![(*i + 1..*j)]
+            },
+            Self::Interior { closing: (i, j),  inner: (p, q) } => { 
+                vec![(*i + 1)..(*p), (*q + 1)..(*j)]
+            },
+            Self::Multibranch { closing: (i, j), branches } => {
+                let mut result = vec![];
+                let mut start = *i;
+                for &(p, q) in branches {
+                    result.push((start+1)..p);
+                    start = q;
+                }
+                result.push((start+1)..(*j));
+                result
+            }
+            Self::Exterior { branches } => {
+                let mut result = Vec::new();
+                let mut start = 0;
+                for &(p, q) in branches {
+                    result.push(start..p);
+                    start = q+1;
+                }
+                result.push(start..len);
+                result
+            }
+        }
+    }
+
+    pub fn unpaired_indices(&self, len: usize) -> Vec<usize> {
+        self.unpaired_ranges(len)
+            .into_iter()
+            .flat_map(|r| r.collect::<Vec<_>>())
+            .collect()
+    }
+
+    /// Returns all sequence indices that should point to this loop.
+    pub fn inclusive_unpaired_indices(&self, len: usize) -> Vec<usize> {
+        self.unpaired_ranges(len-1)
+            .into_iter()
+            .map(|r| r.start..=r.end)
+            .flat_map(|r| r.collect::<Vec<_>>())
+            .collect()
+    }
+   
+    /// Split the given loop into two new loops at the indices i,j
+    /// NOTE: Returns (outer, inner)
+    pub fn split_loop(&self, i: usize, j: usize) -> (Self, Self) {
+        assert!(i < j, "Split pair (i,j) must satisfy i < j");
+        match self {
+            Self::Hairpin { closing: (a, b) } => {
+                assert!(*a < i && j < *b, "Pair (i,j) must be within hairpin loop");
+                (Self::Interior { closing: (*a, *b), inner: (i, j), },
+                 Self::Hairpin { closing: (i, j), })
+            }
+            
+            Self::Interior { closing: (a, b), inner: (p, q) } => {
+                assert!(*a < i && j < *b, "Pair (i,j) outside of loop");
+                assert!(!(*p < i && j < *q), "Pair (i,j) outside of loop");
+
+                if i < *p && *q < j {
+                    (Self::Interior { closing: (*a, *b), inner: (i, j) },
+                     Self::Interior { closing: (i, j), inner: (*p, *q) })
+                } else if j < *p {
+                    (Self::Multibranch { closing: (*a, *b), branches: vec![(i, j), (*p, *q)] },
+                     Self::Hairpin { closing: (i, j) })
+                } else if *q < i {
+                    (Self::Multibranch { closing: (*a, *b), branches: vec![(*p, *q), (i, j)] },
+                     Self::Hairpin { closing: (i, j) })
+                } else {
+                    panic!("that really should not happen.");
+                }
+            }
+
+            Self::Multibranch { closing: (a, b), branches } => {
+                assert!(*a < i && j < *b, "Pair (i,j) outside loop");
+
+                let mut outer_branches = vec![(i, j)];
+                let mut inner_branches = vec![];
+    
+                for &(p, q) in branches {
+                    assert!(p < q);
+                    if j < p || q < i {
+                        outer_branches.push((p, q));
+                    } else { 
+                        assert!(i < p && q < j);
+                        inner_branches.push((p, q));
+                    } 
+                }
+
+                outer_branches.sort_unstable();
+                inner_branches.sort_unstable();
+                (Self::classify(Some((*a, *b)), outer_branches),
+                 Self::classify(Some((i, j)), inner_branches))
+            }
+
+            Self::Exterior { branches } => {
+                let mut outer_branches = vec![];
+                let mut inner_branches = vec![];
+    
+                outer_branches.push((i, j));
+                for &(p, q) in branches {
+                    assert!(p < q);
+                    if j < p || q < i {
+                        outer_branches.push((p, q));
+                    } else { 
+                        assert!(i < p && q < j);
+                        inner_branches.push((p, q));
+                    } 
+                }
+
+                outer_branches.sort_unstable();
+                inner_branches.sort_unstable();
+                (Self::classify(None, outer_branches),
+                 Self::classify(Some((i, j)), inner_branches))
+            }
+        }
+    }
+
+    /// Join two loops by reference. 
+    /// NOTE: the outer loop must be joined with the inner loop!
+    pub fn join_loop(&self, other: &Self) -> Self {
+        match (self, other) {
+            (Self::Hairpin { .. }, _) => { 
+                panic!("A hairpin cannot be the outer loop!");
+            }
+
+            (_, Self::Exterior { .. }) => { 
+                panic!("Multi-stranded moves are not supported.");
+            }
+
+            (Self::Interior { closing: outer_closing, inner },
+             Self::Hairpin { closing: inner_closing }) => {
+                assert_eq!(inner, inner_closing, "Cannot join interior & haipin loops!");
+                Self::Hairpin { closing: *outer_closing } 
+            }
+
+            (Self::Interior { closing: outer_closing, inner: outer_inner },
+             Self::Interior { closing: inner_closing, inner: inner_inner })  => {
+                assert_eq!(outer_inner, inner_closing, "Cannot join interior & interior loops!");
+                Self::Interior { closing: *outer_closing, inner: *inner_inner }
+            }
+ 
+            (Self::Interior { closing: outer_closing, inner },
+             Self::Multibranch { closing: inner_closing, branches }) => {
+                assert_eq!(inner, inner_closing, "Cannot join interior & multibranch loops!");
+                Self::Multibranch { closing: *outer_closing, branches: branches.clone() }
+            },
+
+            (Self::Multibranch { closing: outer_closing, branches },
+             Self::Hairpin { closing: inner_closing }
+            ) => {
+                let new_branches: Vec<_> = branches.iter().cloned()
+                    .filter(|b| b != inner_closing)
+                    .collect();
+                assert_eq!(branches.len(), new_branches.len() + 1, "Cannot join multibranch & hairpin loops!");
+                Self::classify(Some(*outer_closing), new_branches)
+            }
+  
+            (Self::Multibranch { closing: outer_closing, branches },
+             Self::Interior { closing: inner_closing, inner }
+            ) => {
+                let new_branches: Vec<_>  = branches.iter().cloned()
+                    .map(|x| if x == *inner_closing { *inner } else { x })
+                    .collect();
+                Self::Multibranch { closing: *outer_closing, branches: new_branches }
+            },
+ 
+            (Self::Multibranch { closing: outer_closing, branches: outer_branches },
+             Self::Multibranch { closing: inner_closing, branches: inner_branches }
+            ) => {
+                let new_branches: Vec<_>  = outer_branches.iter().cloned()
+                    .flat_map(|x| if x == *inner_closing { inner_branches.clone()  } else { vec![x] })
+                    .collect();
+                Self::Multibranch { closing: *outer_closing, branches: new_branches }
+            },
+            
+            (Self::Exterior { branches },
+             Self::Hairpin { closing: inner_closing }
+            ) => {
+                let new_branches: Vec<_>  = branches.iter().cloned()
+                    .filter(|b| b != inner_closing)
+                    .collect();
+                Self::Exterior { branches: new_branches }
+            }
+
+            (Self::Exterior { branches },
+             Self::Interior { closing: inner_closing, inner }
+            ) => {
+                let new_branches: Vec<_>  = branches.iter().cloned()
+                    .map(|x| if x == *inner_closing { *inner } else { x })
+                    .collect();
+                Self::Exterior { branches: new_branches }
+            },
+              
+            (Self::Exterior { branches: outer_branches },
+             Self::Multibranch { closing: inner_closing, branches: inner_branches }
+            ) => {
+                let new_branches: Vec<_>  = outer_branches.iter().cloned()
+                    .flat_map(|x| if x == *inner_closing { inner_branches.clone()  } else { vec![x] })
+                    .collect();
+                Self::Exterior { branches: new_branches }
             },
         }
     }
@@ -133,17 +350,58 @@ mod tests {
     }
 
     #[test]
-    fn test_decompose_loops_hairpin() {
-        let dbn = ".(...).";
-
-        let eloop = NearestNeighborLoop::Exterior { 
-            branches: vec![(1, 5)], 
-        };
-
+    fn test_loop_ranges() {
         let hloop = NearestNeighborLoop::Hairpin { 
             closing: (1, 5) 
         };
+        println!("{:?}", hloop.unpaired_ranges(26));
+        println!("{:?}", hloop.unpaired_indices(26));
+        println!("{:?}", hloop.inclusive_unpaired_indices(26));
 
+        let iloop = NearestNeighborLoop::Interior { 
+            closing: (1, 9),
+            inner: (2, 8) 
+        };
+        println!("{:?}", iloop.unpaired_ranges(26));
+        println!("{:?}", iloop.unpaired_indices(26));
+        println!("{:?}", iloop.inclusive_unpaired_indices(26));
+ 
+        let iloop = NearestNeighborLoop::Interior { 
+            closing: (1, 9),
+            inner: (2, 7) 
+        };
+        println!("{:?}", iloop.unpaired_ranges(26));
+        println!("{:?}", iloop.unpaired_indices(26));
+        println!("{:?}", iloop.inclusive_unpaired_indices(26));
+ 
+
+        let mloop = NearestNeighborLoop::Multibranch { 
+            closing: (1, 15),
+            branches: vec![(2, 4), (5, 9)],
+        };
+        println!("{:?}", mloop.unpaired_ranges(26));
+        println!("{:?}", mloop.unpaired_indices(26));
+        println!("{:?}", mloop.inclusive_unpaired_indices(26));
+
+        let eloop = NearestNeighborLoop::Exterior { 
+            branches: vec![(1, 5), (6, 11)], 
+        };
+        println!("{:?}", eloop.unpaired_ranges(26));
+        println!("{:?}", eloop.unpaired_indices(26));
+        println!("{:?}", eloop.inclusive_unpaired_indices(26));
+
+    }
+
+
+    #[test]
+    fn test_decompose_loops_hairpin() {
+        let dbn = ".(...).";
+        let eloop = NearestNeighborLoop::Exterior { 
+            branches: vec![(1, 5)], 
+        };
+        let hloop = NearestNeighborLoop::Hairpin { 
+            closing: (1, 5) 
+        };
         let loops = PairTable::try_from(dbn).expect("valid").loops();
         println!("{:?}", loops);
         assert!(loops.len() == 2);
--- crates/kinetics/src/loop_structure.rs
+++ crates/kinetics/src/loop_structure.rs
@@ -8,15 +8,17 @@ use crate::EnergyModel;
 use crate::utils::Base;
 
 pub struct LoopCache {
+    eval_loop: FxHashMap<NearestNeighborLoop, i32>,
     add_pairs: FxHashMap<NearestNeighborLoop, Vec<(NearestNeighborLoop, NearestNeighborLoop, i32)>>,
 }
 
 #[derive(Debug)]
 pub struct LoopStructure {
     loop_list: IntMap<usize, (NearestNeighborLoop, i32)>,
-    l_indices: IntSet<usize>,
+    l_indices: IntSet<usize>, // holds free usize for loop_list & loop_neighbors
     
-    pair_list: Vec<(usize, usize)>, // 0-indexed pairs
+    pair_list: IntMap<usize, (usize, usize)>,
+    p_indices: IntSet<usize>,
  
     // sequence index to loop_list_index 
     // for unpaired: all i of same loop point to same loop
@@ -28,45 +30,194 @@ pub struct LoopStructure {
     pair_neighbors: IntMap<usize, i32>, // pair_list index to deltaE
 }
 
+impl LoopStructure {
+    pub fn get_add_neighbors(&self) -> Vec<(usize, usize, i32)> {
+        self.loop_neighbors
+            .values()
+            .flat_map(|v| v.iter().cloned())
+            .collect()
+    }
+
+    pub fn get_del_neighbors(&self) -> Vec<(usize, (usize, usize), i32)> {
+        self.pair_neighbors
+            .iter()
+            .map(|(pli, &delta_e)| (*pli, self.pair_list[pli], delta_e))
+            .collect()
+    }
+
+    pub fn energy(&self) -> i32 {
+        self.loop_list
+            .values()
+            .map(|(_, e)| *e)
+            .sum()
+    }
+
+    /// Produce a new updated SecStruct.
+    pub fn apply_del_move<E: EnergyModel>(&mut self, pl_idx: usize, sequence: &[Base], model: &E) {
+        let &(i, j) = self.pair_list.get(&pl_idx).expect("Missing pair_list entry.");
+
+        let &lli = self.loop_lookup.get(&i).expect("Missing loop_lookup entry for i.");
+        let &llj = self.loop_lookup.get(&j).expect("Missing loop_lookup entry for j.");
+
+        let (outer, o_en) = self.loop_list.get(&lli).expect("Missing loop_list entry lli.");
+        let (inner, i_en) = self.loop_list.get(&llj).expect("Missing loop_list entry llj.");
+
+        // Recompute or get from global cache.
+        let combo = &outer.join_loop(&inner);
+        let delta = self.pair_neighbors.get(&pl_idx).expect("Missing pair_neighbors entry.");
+        let combo_energy = (o_en + i_en) - delta;
+
+        // Cleanup and re-allocate.
+        self.pair_list.remove(&pl_idx);
+        self.pair_neighbors.remove(&pl_idx);
+        self.p_indices.insert(pl_idx);
+
+        self.loop_list.remove(&lli);
+        self.loop_list.remove(&llj);
+        self.loop_neighbors.remove(&lli);
+        self.loop_neighbors.remove(&llj);
+        self.l_indices.insert(llj);
+
+        self.loop_list.insert(lli, (combo.clone(), combo_energy));
+        let neighbors = get_loop_neighbors(&combo, &combo_energy, sequence, model);
+        self.loop_neighbors.insert(lli, neighbors);
+
+        for k in &combo.inclusive_unpaired_indices(sequence.len()) {
+            assert!(self.loop_lookup[k] == lli || self.loop_lookup[k] == llj);
+            self.loop_lookup.insert(*k, lli);
+        }
+    }
+
+    /// Produce a new updated SecStruct.
+    pub fn apply_add_move<E: EnergyModel>(&mut self, i: usize, j: usize, 
+        sequence: &[Base], model: &E
+    ) {
+        let &lli = self.loop_lookup.get(&i).expect("Missing loop_lookup entry for i.");
+
+        assert_eq!(
+            lli, 
+            *self.loop_lookup.get(&j).expect("Missing loop_lookup entry for j.")
+        );
+
+        let (combo, c_energy) = self.loop_list.get(&lli).expect("Missing loop_list entry lli.");
+        let (outer, inner) = combo.split_loop(i, j);
+        //NOTE; could look that up.
+        let outer_energy = model.energy_of_loop(sequence, &outer);
+        let inner_energy = model.energy_of_loop(sequence, &inner);
+        let delta = (outer_energy + inner_energy) - c_energy;
+  
+        self.loop_list.remove(&lli);
+        self.loop_neighbors.remove(&lli);
+        self.loop_list.insert(lli, (outer.clone(), outer_energy));
+        self.loop_neighbors.insert(lli, 
+            get_loop_neighbors(&outer, &outer_energy, sequence, model));
+
+        let llj = if let Some(&x) = self.l_indices.iter().next() {
+            self.l_indices.remove(&x); x
+        } else {
+            self.loop_list.len()
+        };
+
+        self.loop_list.insert(llj, (inner.clone(), inner_energy));
+        self.loop_neighbors.insert(llj, 
+            get_loop_neighbors(&inner, &inner_energy, sequence, model));
+
+        let pli = if let Some(&x) = self.p_indices.iter().next() {
+            self.p_indices.remove(&x); x
+        } else {
+            self.pair_list.len()
+        };
+        self.pair_list.insert(pli, (i, j));
+
+        for k in &outer.inclusive_unpaired_indices(sequence.len()) {
+            self.loop_lookup.insert(*k, lli);
+        }
+
+        for k in &inner.inclusive_unpaired_indices(sequence.len()) {
+            self.loop_lookup.insert(*k, llj);
+        }
+
+        self.pair_neighbors.insert(pli, delta);
+
+    }
+
+}
+
+impl<T: LoopDecomposition, M: EnergyModel> TryFrom<(&[Base], &T, &M)> for LoopStructure {
+    type Error = String;
+
+    fn try_from((sequence, pairings, model): (&[Base], &T, &M)) -> Result<Self, Self::Error> {
+        let mut loop_list = IntMap::default();
+        let mut pair_list = IntMap::default();
+        let mut loop_lookup = IntMap::default();
+        let mut ll_idx = 0;
+        let mut pl_idx = 0;
+
+        pairings.for_each_loop(|l| {
+            let energy = model.energy_of_loop(sequence, l);
+            loop_list.insert(ll_idx, (l.clone(), energy));
+            if let Some((i, j)) = l.closing() {
+                pair_list.insert(pl_idx, (i, j)); 
+                pl_idx += 1;
+            }
+            for k in &l.inclusive_unpaired_indices(sequence.len()) {
+                loop_lookup.insert(*k, ll_idx);
+            }
+            ll_idx += 1;
+        });
+
+        let mut loop_neighbors = IntMap::default();
+        for (nn_idx, (nn_loop, nn_energy)) in loop_list.iter() {
+            let neighbors = get_loop_neighbors(&nn_loop, nn_energy, sequence, model);
+            loop_neighbors.insert(*nn_idx, neighbors);
+        }
+
+        let mut pair_neighbors = IntMap::default();
+        for (pl_idx, (i, j)) in pair_list.iter() {
+            let (outer, outer_energy) = loop_list.get(loop_lookup.get(i).unwrap()).unwrap();
+            let (inner, inner_energy) = loop_list.get(loop_lookup.get(j).unwrap()).unwrap();
+            let combo = &outer.join_loop(&inner);
+            let combo_energy = model.energy_of_loop(sequence, &combo);
+            let delta = combo_energy - (outer_energy + inner_energy);
+            pair_neighbors.insert(*pl_idx, delta);
+        }
+
+        Ok(LoopStructure {
+            loop_list,
+            l_indices: IntSet::default(),
+            pair_list,
+            p_indices: IntSet::default(),
+            loop_lookup,
+            loop_neighbors,
+            pair_neighbors,
+        })
+    }
+
+}
+
+fn get_loop_neighbors<E: EnergyModel>(
+    nn_loop: &NearestNeighborLoop, 
+    nn_energy: &i32, 
+    sequence: &[Base], 
+    model: &E
+) -> Vec<(usize, usize, i32)> {
+    let mut result = vec![];
+    for (i, j) in unpaired_pairs_in_loop(nn_loop, sequence, model) {
+        let (outer, inner) = nn_loop.split_loop(i, j);
+        let outer_energy = model.energy_of_loop(sequence, &outer);
+        let inner_energy = model.energy_of_loop(sequence, &inner);
+        let delta = (outer_energy + inner_energy) - nn_energy;
+        result.push((i, j, delta));
+    }
+    result
+}
+
 pub fn unpaired_pairs_in_loop<E: EnergyModel>(
     nn_loop: &NearestNeighborLoop,
     sequence: &[Base],
     model: &E,
 ) -> Vec<(usize, usize)> {
-
-    let unpaired: Vec<usize> = match nn_loop {
-        NearestNeighborLoop::Hairpin { closing: (i, j) } => {
-            (*i + 1..*j).collect()
-        }
-        NearestNeighborLoop::Interior { closing: (i, j), inner: (k, l) } => {
-            let mut v = Vec::new();
-            v.extend(*i + 1..*k);
-            v.extend(*l + 1..*j);
-            v
-        }
-        NearestNeighborLoop::Multibranch { closing: (i, j), branches } => {
-            let mut v = Vec::new();
-            let mut start = *i;
-            for &(p, q) in branches {
-                v.extend(start + 1..p);
-                start = q;
-            }
-            v.extend(start+1..=*j);
-            v
-        }
-        NearestNeighborLoop::Exterior { branches } => {
-            let mut v = Vec::new();
-            let mut start = 0;
-            v.push(start);
-            for &(p, q) in branches {
-                v.extend(start+1..p);
-                start = q;
-            }
-            v.extend(start+1..sequence.len());
-            v
-        }
-    };
-
+    let unpaired = nn_loop.unpaired_indices(sequence.len());
     // Pair all (i, j) with j > i + 3 and check if pairable
     let mut pairs = Vec::new();
     for (idx_i, &i) in unpaired.iter().enumerate() {
@@ -79,100 +230,9 @@ pub fn unpaired_pairs_in_loop<E: EnergyModel>(
             }
         }
     }
-
     pairs
 }
 
-
-
-impl LoopStructure {
-    /// Produce a new updated SecStruct.
-    pub fn apply_move(&mut self, i: usize, j: usize) {
-        todo!("modify in place, depends on loop_neighbors and pair_neighbors.");
-    }
-
-    fn get_loop_neighbors<E: EnergyModel>(&mut self, cache: Option<LoopCache>, sequence: &[Base], model: &E) {
-        for (idx, (nn_loop, en)) in self.loop_list.iter() {
-            let pairs = unpaired_pairs_in_loop(nn_loop, sequence, model);
-            println!("{:?}", pairs);
-        }
-    }
-    
-
-}
-
-
-impl<T: LoopDecomposition, M: EnergyModel> TryFrom<(&[Base], &T, &M)> for LoopStructure {
-    type Error = String;
-
-    fn try_from((sequence, pairings, model): (&[Base], &T, &M)) -> Result<Self, Self::Error> {
-        let mut loop_list = IntMap::default();
-        let mut pair_list = Vec::new();
-        let mut loop_lookup = IntMap::default();
-        let mut idx = 0;
-
-        pairings.for_each_loop(|l| {
-            let energy = model.energy_of_loop(sequence, l);
-            loop_list.insert(idx, (l.clone(), energy));
-
-            match l {
-                NearestNeighborLoop::Hairpin { closing: (i, j) } => {
-                    pair_list.push((*i, *j));
-                    for k in *i+1..=*j {
-                        loop_lookup.insert(k, idx);
-                    }
-                }
-                NearestNeighborLoop::Interior { closing: (i, j),  inner: (p, q) } => { 
-                    pair_list.push((*i, *j));
-                    for k in *i+1..=*p {
-                        loop_lookup.insert(k, idx);
-                    }
-                    for k in *q+1..=*j {
-                        loop_lookup.insert(k, idx);
-                    }
-                }
-                NearestNeighborLoop::Multibranch { closing: (i, j), branches } => {
-                    pair_list.push((*i, *j));
-                    let mut start = *i;
-                    for &(p, q) in branches {
-                        for k in start+1..=p {
-                            loop_lookup.insert(k, idx);
-                        }
-                        start = q;
-                    }
-                    for k in start+1..=*j {
-                        loop_lookup.insert(k, idx);
-                    }
-                }
-                NearestNeighborLoop::Exterior { branches } => {
-                    let mut start = 0;
-                    loop_lookup.insert(start, idx);
-                    for &(p, q) in branches {
-                        for k in start+1..=p {
-                            loop_lookup.insert(k, idx);
-                        }
-                        start = q;
-                    }
-                    for k in start+1..sequence.len() {
-                        loop_lookup.insert(k, idx);
-                    }
-                }
-            }
-            idx += 1;
-
-        });
-
-        Ok(LoopStructure {
-            loop_list,
-            l_indices: IntSet::default(),
-            pair_list,
-            loop_lookup,
-            loop_neighbors: IntMap::default(),
-            pair_neighbors: IntMap::default(),
-        })
-    }
-
-}
 
 
 #[cfg(test)]
@@ -180,6 +240,7 @@ mod tests {
     use super::*;
     use crate::ViennaRNA;
     use structure::PairTable;
+    use crate::utils::basify;
 
     #[test]
     fn test_loop_structure() {
@@ -188,16 +249,63 @@ mod tests {
         let struct_1 = ".(....)....";
 
         let model = ViennaRNA::new();
-        let sequence: Vec<Base> = sequence.chars().map(Base::try_from).collect::<Result<_, _>>().unwrap();
+        let sequence = basify(sequence);
         let struct_0 = PairTable::try_from(struct_0).expect("valid");
-        let ls = LoopStructure::try_from((&sequence[..], &struct_0, &model)).expect("bla");
-        println!("{:?}", ls);
+        let mut ls = LoopStructure::try_from((&sequence[..], &struct_0, &model)).expect("bla");
+
+        println!("{:?} {}", ls, ls.energy());
+        println!("Add neighbors");
+        for (i, j, de) in ls.get_add_neighbors() {
+            println!("{} {} {}", i, j, de);
+        }
+
+        println!("Del neighbors");
+        for (pi, (i, j), de) in ls.get_del_neighbors() {
+            println!("{} ({}, {}), {}", pi, i, j, de);
+        }
+
+        println!("Adddig 1, 5");
+        ls.apply_add_move(1, 5, &sequence[..], &model);
+        println!("{:?} {}", ls, ls.energy());
+        println!("Add neighbors");
+        for (i, j, de) in ls.get_add_neighbors() {
+            println!("{} {} {}", i, j, de);
+        }
+
+        println!("Del neighbors");
+        for (pi, (i, j), de) in ls.get_del_neighbors() {
+            println!("{} ({}, {}), {}", pi, i, j, de);
+        }
+
+        println!("Removing 1, 5");
+        ls.apply_del_move(0, &sequence[..], &model);
+        println!("{:?} {}", ls, ls.energy());
+        println!("Add neighbors");
+        for (i, j, de) in ls.get_add_neighbors() {
+            println!("{} {} {}", i, j, de);
+        }
+
+        println!("Del neighbors");
+        for (pi, (i, j), de) in ls.get_del_neighbors() {
+            println!("{} ({}, {}), {}", pi, i, j, de);
+        }
+
 
 
         let struct_1 = PairTable::try_from(struct_1).expect("valid");
-        let mut ls = LoopStructure::try_from((&sequence[..], &struct_0, &model)).expect("bla");
-        println!("{:?}", ls);
-        ls.get_loop_neighbors(None, &sequence, &model);
+        let mut ls = LoopStructure::try_from((&sequence[..], &struct_1, &model)).expect("bla");
+        println!("Add neighbors");
+        for (i, j, de) in ls.get_add_neighbors() {
+            println!("{} {} {}", i, j, de);
+        }
+
+        println!("Del neighbors");
+        for (pi, (i, j), de) in ls.get_del_neighbors() {
+            println!("{} ({}, {}), {}", pi, i, j, de);
+        }
+
+
+        //println!("{:?} {}", ls, ls.energy());
 
         assert!(false);
     }
